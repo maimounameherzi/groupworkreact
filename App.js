84b32450bfef39305a1a156b138602f9ede1a28a
maimounameherzi <102535877+maimounameherzi@users.noreply.github.com>
2022-04-27 14:57:02 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CustomNavBar from './components/CustomNabBar/CustomNavBar';
+
+import CustomTable from './components/Table/CustomTable';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomNavBar/>
+      <CustomTable/>
+      
+    </div>
+  );
+}
+
+export default App;
